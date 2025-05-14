4761c042f437f4931a375a35fb171dde18f27206
DamSha <chauvet.damien@gmail.com>
2025-05-14 10:18:24 -0400

    gh-pages settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    root: 'src',
+    base: 'DamSha',
+    build: {
+        outDir: 'build',
+        emptyOutDir: true, // also necessary
+    }
 })
